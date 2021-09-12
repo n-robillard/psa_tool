aa96622b34adf194e92307c099385e4740b8bcc9
Nicolas Robillard <nicolas.robillard@ymail.com>
2021-09-12 23:31:26 +0200

    parsing fasta and make it in the right form

--- src/pb_parsing.py
+++ src/pb_parsing.py
@@ -1,5 +1,10 @@
 from Bio import SeqIO
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pb_analyze as pb
+from scipy import stats
 
 pathToFile = open("./data/sequences.PB.fasta")
 
@@ -11,4 +16,20 @@ def parse_fasta(pathToFile):
     return pb_seq
 
 def transpose_fasta(sequences):
-    pb_seq = pb_seq.transpose()
+    sequences = sequences.transpose()
+    return sequences
+
+
+test = parse_fasta(pathToFile)
+#print(f"la matrice original est celle-ci : \n {test}")
+test1 = transpose_fasta(test)
+#print(f"la matrice transposée est celle-ci : \n {test1}")
+#shape_array = np.shape(test1)
+#print(shape_array)
+test2 = test1[2:-2]
+#print(f"La matrice trim des 2 premier et 2 derniers éléments : \n {test2}")
+
+MI,eeMI = pb.calculate_MI_global(test2)
+print(MI)
+print(eeMI)
+print(np.shape(MI))
